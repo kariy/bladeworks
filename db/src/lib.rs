7c7867fce865148a860c60f6f5bbb9378d0448aa
Ammar Arif <evergreenkary@gmail.com>
2024-07-09 18:16:16 -0400

    wip

--- db/src/lib.rs
+++ db/src/lib.rs
@@ -1,14 +1,35 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use bladeworks_context::Ctx;
+use katana_db::mdbx::DbEnv;
+use katana_primitives::{
+    class::ClassHash,
+    contract::{ContractAddress, Nonce, StorageKey, StorageValue},
+};
+use katana_provider::{traits::state::StateProvider, ProviderResult};
+
+pub struct Db {
+    ctx: Ctx,
+    db: DbEnv,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+// actual_storage_key = tenant_id + key
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+impl StateProvider for Db {
+    fn storage(
+        &self,
+        address: ContractAddress,
+        storage_key: StorageKey,
+    ) -> ProviderResult<Option<StorageValue>> {
+        todo!()
+    }
+
+    fn class_hash_of_contract(
+        &self,
+        address: ContractAddress,
+    ) -> ProviderResult<Option<ClassHash>> {
+        todo!()
+    }
+
+    fn nonce(&self, address: ContractAddress) -> ProviderResult<Option<Nonce>> {
+        todo!()
     }
 }
